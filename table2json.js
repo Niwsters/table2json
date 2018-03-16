a882fe12a06dbafd85ed24360d13ac86a0616676
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2018-03-16 14:30:22 +0100

    Initializing repository

---
+++ table2json.js
@@ -0,0 +1,32 @@
+// Credit to ccheney on GitHub: https://gist.github.com/ccheney/4389114
+// Edits by Niwsters
+
+var table2json = function (htmlStr) { 
+  // Parse given HTML string to a jQuery object
+  var $table = $($.parseHTML($.trim(htmlStr)));
+
+  // Store all the headers to use as column names
+  var headers = [];
+  $table.find('th').each(function (i, n) {
+    var $header = $(n);
+    headers.push($header.text());
+  });
+
+  // Read all rows and put in a JSON object
+  var rows = [];
+  $table.find('tr').each(function (i, n) {
+    var $row = $(n);
+
+    var cols = {};
+    for(var j in headers) {
+      cols[headers[j]] = $row.find('td:eq(' + j + ')').text();
+    }
+    rows.push(cols);
+  });
+
+  // Remove first row because it's the row with headers
+  rows.shift();
+
+  // Convert JSON object to string and return
+  return JSON.stringify(rows);
+};
